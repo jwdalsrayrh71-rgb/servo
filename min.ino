ee903b808674404330762091029ff706585d6ec3
jwdalsrayrh71-rgb <jwdalsrayrh71@gmail.com>
2025-10-06 10:32:09 +0300

    Create min.ino

---
+++ min.ino
@@ -0,0 +1,23 @@
+include "servo"
+#include <Servo.h>
+
+Servo myservo;  // إنشاء كائن للسيرفو
+
+void setup() {
+  myservo.attach(9);  // توصيل السيرفو إلى المنفذ رقم 9
+}
+
+void loop() {
+  // تحريك السيرفو من 0 إلى 180 درجة
+  for (int angle = 0; angle <= 180; angle++) {
+    myservo.write(angle);
+    delay(15); // تأخير بسيط لإعطاء الوقت للسيرفو للتحرك
+  }
+
+  // ثم الرجوع من 180 إلى 0 درجة
+  for (int angle = 180; angle >= 0; angle--) {
+    myservo.write(angle);
+    delay(15);
+  }
+}
+
